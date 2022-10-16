58ddfec4a3a3682aa93971e3b8bef9f91f524604
Jett Thompson <jett.thompson.59@gmail.com>
2022-10-16 12:34:54 +1100

    change to putting the output file in /tmp so it works and wrapp  in try catch

--- handler-load-test.ts
+++ handler-load-test.ts
@@ -9,41 +9,46 @@ interface Options {
 }
 
 export async function handler(options: Options, context: Context) {
-    console.log("Running load test");
-    if (!options.url) {
-        throw new Error('URL not specified');
-    }
-    if (!options.uploadPath) {
-        throw new Error('Upload path not specified');
-    }
-    console.log("Input is valid");
-    const s3 = new S3();
-    const key = options.uploadPath;
-    const bucket = process.env.BUCKET_NAME;
-    if (!bucket) {
-        throw new Error('Bucket name not specified');
-    }
-    console.log("Running k6 now!");
-    const child = execFile('k6', ['run', '-e', `URL=${options.url}`, '-e', `FILE=${key}`, 'k6.js'], (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
+    try {
+        console.log("Running load test");
+        if (!options.url) {
+            throw new Error('URL not specified');
         }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
-    await new Promise<void>((resolve) => {
-        child.on('exit', () => {
-            resolve();
+        if (!options.uploadPath) {
+            throw new Error('Upload path not specified');
+        }
+        console.log("Input is valid");
+        const s3 = new S3();
+        const key = options.uploadPath;
+        const bucket = process.env.BUCKET_NAME;
+        if (!bucket) {
+            throw new Error('Bucket name not specified');
+        }
+        console.log("Running k6 now!");
+        const child = execFile('k6', ['run', '-e', `URL=${options.url}`, '-e', `FILE=/tmp/${key}`, 'k6.js'], (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
         });
-    });
-    console.log("Uploading");
-    const uploadParams = {
-        Bucket: bucket,
-        Key: key,
-        Body: readFileSync(key),
-    };
+        await new Promise<void>((resolve) => {
+            child.on('exit', () => {
+                resolve();
+            });
+        });
+        console.log("Uploading");
+        const uploadParams = {
+            Bucket: bucket,
+            Key: key,
+            Body: readFileSync(`/tmp/${key}`),
+        };
 
-    const upload = await s3.upload(uploadParams).promise();
-    console.log(`Upload complete: ${upload.Location}`);
+        const upload = await s3.upload(uploadParams).promise();
+        console.log(`Upload complete: ${upload.Location}`);
+    } catch (err) {
+        console.error(err);
+        // Pretend all is well so we don't get retried.
+    }
 }
