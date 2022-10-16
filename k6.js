24042305f801327bd82f8789909e2edb584df555
Luke Thompson <endeavour9@gmail.com>
2022-10-16 21:52:16 +1100

    Use our reporter

--- k6.js
+++ k6.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import {check, sleep} from "k6";
-import {htmlReport} from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+//import {htmlReport} from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import {htmlReport} from "https://raw.githubusercontent.com/cleancode-app/k6-reporter/main/dist/bundle.js";
 
 export let options = {
     stages: [
@@ -12,7 +13,10 @@ export let options = {
 
 export function handleSummary(data) {
     return {
-        [__ENV.FILE]: htmlReport(data),
+        [__ENV.FILE]: htmlReport(data, {
+            title: `CleanCode Load Test ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`, 
+            url: __ENV.URL
+        }),
     };
 }
 
